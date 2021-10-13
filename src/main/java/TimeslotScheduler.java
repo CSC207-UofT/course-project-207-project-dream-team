2a7ad5c34729f1979bdd601135e9dd2545b2e339
oscarchen <zhonghan.chen@mail.utoronto.ca>
2021-10-12 22:41:02 -0400

    cleaned

---
+++ src/main/java/TimeslotScheduler.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class TimeslotScheduler {
+    PreferenceManager pm;
+    int y_n = pm.getPrefer_timeslot();//1 or -1
+    ArrayList<Timeslot> timeslot;//exact timeslot(s)
+
+    static ArrayList<Course[]> Ins_Select(ArrayList<Course[]> input, int y_n, ArrayList<Timeslot> timeslot) {
+        //The return type can also be ArrayList<String[]> if we choose to return the course codes only.
+        ArrayList<Course[]> output = new ArrayList<>();
+        //some operations
+        return output;
+    }
+}
