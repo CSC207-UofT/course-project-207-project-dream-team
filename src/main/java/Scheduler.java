ba2cbd3bb3ef40be7dcf287f13672d0dacce8688
PIAO-A-PIAO <piaoxuanyi2001@163.com>
2021-10-13 16:55:21 -0400

    piao: edited class Scheduler and created class Timetable

--- src/main/java/Scheduler.java
+++ src/main/java/Scheduler.java
@@ -1,2 +1,13 @@
-public class Scheduler {
+import java.util.ArrayList;
+
+public abstract class Scheduler {
+    ArrayList<Course[]> courses;//courses ready to be arranged
+
+    public Scheduler(ArrayList<Course[]> courses){
+        this.courses = courses;
+    }
+
+    public abstract Timetable Arrange(){
+/
+    }
 }
