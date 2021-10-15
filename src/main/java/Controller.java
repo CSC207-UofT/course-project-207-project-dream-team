b3f531af0505e218bc72fe6cc06dcba32cd574a8
PIAO-A-PIAO <53113352+PIAO-A-PIAO@users.noreply.github.com>
2021-10-15 18:54:30 -0400

    Add files via upload

---
+++ src/main/java/Controller.java
@@ -0,0 +1,31 @@
+import java.time.DayOfWeek;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Controller {
+    //variables
+    ArrayList<Course> courseList;
+
+
+    //We don't need a constructor right?
+
+    //methods
+
+    //run
+    public static void main(String[] args) {
+        //for each course instance
+        String[] ask = {"course code", "type", "instructor", "day of week",
+        "start time", "end time"};
+        Scanner sc = new Scanner(System.in);
+        String[] inputs = new String[6];
+        for (int i = 0; i < inputs.length; i++)
+        {
+            System.out.println("What is the " + ask[i] + " of this course?");
+            inputs[i] = sc.nextLine();
+        }
+        Course course = new Course(inputs[0], inputs[1], inputs[2],
+                DayOfWeek.of(Integer.parseInt(inputs[3])), Integer.parseInt(inputs[4]), Integer.parseInt(inputs[5]));
+        System.out.println(course);
+
+    }
+}
