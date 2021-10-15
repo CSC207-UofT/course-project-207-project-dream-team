04dab5cc29ced7b16b3da6b0a081a90aa0832f78
erlewei <lewei.er@mail.utoronto.ca>
2021-10-15 15:56:06 -0400

    Advanced schedulers updated to compile (not complete schedulers)

--- src/main/java/SleepScheduler.java
+++ src/main/java/SleepScheduler.java
@@ -8,5 +8,7 @@ public class SleepScheduler extends Scheduler {
     @Override
     public ArrayList<Timetable> Arrange() {
         //TODO: method to be implemented.
+        ArrayList<Timetable> list = new ArrayList<>();
+        return list;
     }
 }
