8f86e06bf4fd38d54d73bbdf784caea42425b5bc
oscarchen <zhonghan.chen@mail.utoronto.ca>
2021-10-16 19:23:57 -0400

    Test

--- src/test/java/TimetableTest.java
+++ src/test/java/TimetableTest.java
@@ -18,10 +18,14 @@ public class TimetableTest {
                 DayOfWeek.FRIDAY, 12, 17);
         Course course2 = new Course("CSC263", "LEC", "Er",
                 DayOfWeek.THURSDAY, 15, 17);
+        Course course3 = new Course("CSC243", "LEC", "Er",
+                DayOfWeek.THURSDAY, 15, 17);
         // Timetable newTimetable = new Timetable();
         newTimetable.addCourse(course1);
         newTimetable.addCourse(course2);
-        assertFalse(newTimetable.canAdd(course1));
+        assertTrue(newTimetable.canAdd(course1));
+        assertTrue(newTimetable.canAdd(course2));
+
     }
 
 }
