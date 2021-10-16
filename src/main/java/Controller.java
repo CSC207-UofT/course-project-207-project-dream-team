ac498e43ea714d400a480e6c3945a0ca92ca062a
3th4novo <ethanq918@163.com>
2021-10-15 20:00:07 -0400

    Unfinished controller scheduler timetable interaction.

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -6,6 +6,31 @@ public class Controller {
     //variables
     ArrayList<Course> courseList;
 
+    private Course loadCourse() {
+        String[] ask = {"course code", "type", "instructor", "day of week",
+                "start time", "end time"};
+        Scanner sc = new Scanner(System.in);
+        String[] inputs = new String[6];
+        for (int i = 0; i < inputs.length; i++)
+        {
+            System.out.println("What is the " + ask[i] + " of this course?");
+            System.out.println("Please choose from LEC, TUT and LAB.");
+            inputs[i] = sc.nextLine();
+        }
+        Course course = new Course(inputs[0], inputs[1], inputs[2],
+                DayOfWeek.of(Integer.parseInt(inputs[3])), Integer.parseInt(inputs[4]), Integer.parseInt(inputs[5]));
+        this.courseList.add(course);
+        return course;
+    }
+
+    private void schedule() {
+        // In demo, we only choose a simple scheduler to schedule
+        // a small set of courses.
+
+
+    }
+
+
 
     //We don't need a constructor right?
 
@@ -14,18 +39,7 @@ public class Controller {
     //run
     public static void main(String[] args) {
         //for each course instance
-        String[] ask = {"course code", "type", "instructor", "day of week",
-        "start time", "end time"};
-        Scanner sc = new Scanner(System.in);
-        String[] inputs = new String[6];
-        for (int i = 0; i < inputs.length; i++)
-        {
-            System.out.println("What is the " + ask[i] + " of this course?");
-            inputs[i] = sc.nextLine();
-        }
-        Course course = new Course(inputs[0], inputs[1], inputs[2],
-                DayOfWeek.of(Integer.parseInt(inputs[3])), Integer.parseInt(inputs[4]), Integer.parseInt(inputs[5]));
-        System.out.println(course);
+
 
     }
 }
--- src/main/java/TimeslotScheduler.java
+++ src/main/java/TimeslotScheduler.java
@@ -8,7 +8,8 @@ public class TimeslotScheduler extends Scheduler {
     @Override
     public ArrayList<Timetable> arrange(Timetable timetable) {
         //TODO: method to be implemented.
-        ArrayList<Timetable> list = new ArrayList<>();
-        return list;
+        ArrayList<Timetable> resultTimeTable = new ArrayList<>();
+
+        return resultTimeTable;
     }
 }
