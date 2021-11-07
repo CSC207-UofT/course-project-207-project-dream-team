ad944d02bffd1ef7b8a7aa66c6b9a9e3bb9b8c05
oscarchen <zhonghan.chen@mail.utoronto.ca>
2021-11-07 12:41:54 -0500

    Structural Change in File

--- src/main/java/Session.java
+++ src/main/java/Session.java
@@ -8,7 +8,7 @@ public class Session {
                    String courseCode,
                    String sessionCode,
                    Integer[] timeslots){
-        this.instructor = instructor;     //e.g. G
+        this.instructor = instructor;     //e.g. Paul
         this.courseCode = courseCode;     //e.g. CSC207H1F
         this.sessionCode = sessionCode;   //e.g. LEC0101
         this.timeslots = timeslots;       //e.g. [21415, 41315]
