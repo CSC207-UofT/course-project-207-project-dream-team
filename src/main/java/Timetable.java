ad67fdf7ddc45f83969b776e5f403d5bb71ec194
noaoch <jerry.yan@mail.utoronto.ca>
2021-11-08 18:43:17 -0500

    Updated Timetable, added extension method

--- src/main/java/Timetable.java
+++ src/main/java/Timetable.java
@@ -1,4 +1,3 @@
-import java.sql.Time;
 import java.util.*;
 
 public class Timetable {
@@ -23,12 +22,10 @@ public class Timetable {
         this.occupied = new ArrayList<>();
     }
 
-    public Integer[] timeConverter(String timeCode) {
-        int day = Integer.parseInt(timeCode.substring(0, 1));
-        int start_time = Integer.parseInt(timeCode.substring(1, 3));
-        int end_time = Integer.parseInt(timeCode.substring(3, 5));
-        int duration = end_time - start_time;
-        return new Integer[]{day, start_time, end_time, duration};
+    // Overloaded Constructor
+    public Timetable(HashMap<String, Session> timeTable, ArrayList<String> occupied){
+        this.timeTable = timeTable;
+        this.occupied = occupied;
     }
 
     // Check if the timeSpan is empty in timeTable
@@ -41,96 +38,11 @@ public class Timetable {
         return true;
     }
 
-    // Big Function change:  lecCanAdd, tutCanAdd, labCanAdd
-    // return an arraylist that contains all the lecture sessions that can be added to the timetable
-    // Criteria: the session is added IFF all times of the session is currently empty in timeslot
-    // Notice: this is just a checker providing information but making NO CHANGE to the timetable.
-    // lecCanAdd, tutCanAdd, labCanAdd have same scenario.
-    public ArrayList<Session> lecCanAdd(NewCourse course) {
-        ArrayList<Session> availableLEC = new ArrayList<>();     // the output
-        for (Session session : course.lectures) {                // loop all the lecture sessions of this course
-            int count = 0;
-            for (Integer time : session.timeslots) {             // loop timeslots of the session
-                if (!occupied.contains(time.toString()))
-                    count++;
-            }
-            if (count == session.timeslots.length) {
-                availableLEC.add(session);
-            }
-        }
-        return availableLEC;
-    }
 
-    public ArrayList<Session> labCanAdd(NewCourse course) {
-        ArrayList<Session> availableLAB = new ArrayList<>();
-        for (Session session : course.labs) {
-            int count = 0;
-            for (Integer time : session.timeslots) {
-                if (!occupied.contains(time.toString()))
-                    count++;
-            }
-            if (count == session.timeslots.length) {
-                availableLAB.add(session);
-            }
-        }
-        return availableLAB;
-    }
-
-    public ArrayList<Session> tutCanAdd(NewCourse course) {
-        ArrayList<Session> availableTUT = new ArrayList<>();
-        for (Session session : course.tutorials) {
-            int count = 0;
-            for (Integer time : session.timeslots) {
-                if (!occupied.contains(time.toString()))
-                    count++;
-            }
-            if (count == session.timeslots.length) {
-                availableTUT.add(session);
-            }
-        }
-        return availableTUT;
-    }
-
-
-    // Similar function to addCourse, but the structure is different.
-    // Purpose: Add session (lec, tut, lab) to the timetable.
-    // Use number to specify the type to add
-    // 1: lec, 2: tut, 3: lab
-
-//    public void addSession(NewCourse course, int type) {
-//        if (type == 1) {
-//            ArrayList<Session> availSessions = lecCanAdd(course);
-//            Session sessionTOADD = availSessions.get(0);
-//            for (int time : sessionTOADD.timeslots) {
-//                String time1 = String.valueOf(time);
-//                this.timeTable.put(time1, course);
-//                this.occupied.add(time1);
-//            }
-//        }
-//        if (type == 2) {
-//            ArrayList<Session> availSessions = tutCanAdd(course);
-//            Session sessionTOADD = availSessions.get(0);
-//            for (int time : sessionTOADD.timeslots) {
-//                String time1 = String.valueOf(time);
-//                this.timeTable.put(time1, course);
-//                this.occupied.add(time1);
-//
-//            }
-//
-//        }
-//        if (type == 3) {
-//            ArrayList<Session> availSessions = labCanAdd(course);
-//            Session sessionTOADD = availSessions.get(0);
-//            for (int time : sessionTOADD.timeslots) {
-//                String time1 = String.valueOf(time);
-//                this.timeTable.put(time1, course);
-//                this.occupied.add(time1);
-//            }
-//        }
-//    }
-
-    // Return a mapping of course codes to collections of session codes, based on the sessions that are in timetable.
-    // e.g. If timetable has tut0101 and lec0201 for CSC207, then we return {"CSC207": ["TUT0101", "LEC0201"]}.
+    // Return a mapping of course codes to what typeS of sessions are already in the timetable (e.g. Lec, Tut), based on
+    // the sessions that are in timetable.
+    // e.g. If timetable has tut0101 and lec0201 for CSC207, then we return {"CSC207": ["TUT", "LEC"]},
+    // where {} means a dictionary and [] means a set.
     // Precondition: the timetable is set up correctly.
 
     private Map<String, Set<String>> courseToSession(){
@@ -139,10 +51,10 @@ public class Timetable {
 
         for (Session s : sessionsInTimetable){
             if (result.containsKey(s.courseCode)){
-                result.get(s.courseCode).add(s.sessionCode);
+                result.get(s.courseCode).add(s.sessionCode.substring(0, 3));
             }
             else {
-                Set<String> newValue = new HashSet<>(Collections.singleton(s.sessionCode));
+                Set<String> newValue = new HashSet<>(Collections.singleton(s.sessionCode.substring(0, 3)));
                 result.put(s.courseCode, newValue);
             }
         }
@@ -159,27 +71,45 @@ public class Timetable {
             }
         }
         if (TimetableMapping.containsKey(sessionToAdd.courseCode)){
-            return !TimetableMapping.get(sessionToAdd.courseCode).contains(sessionToAdd.sessionCode);
+            return !TimetableMapping.get(sessionToAdd.courseCode).contains(sessionToAdd.sessionCode.substring(0,3));
         }
         return true;
     }
 
     // Precondition: sessionToAdd has no conflict with this timetable.
-    // Add this session.
-    private void addSession(Session sessionToAdd){
+    // Return a new timetable with this session added.
+    private Timetable addSession(Session sessionToAdd){
+        HashMap<String, Session> newMapping = new HashMap<>(this.timeTable);
+        ArrayList<String> newOccupied = new ArrayList<>(this.occupied);
+
         for (int time : sessionToAdd.timeslots){
             String timeString = Integer.toString(time);
-            this.timeTable.put(timeString, sessionToAdd);
-            this.occupied.add(timeString);
+            newMapping.put(timeString, sessionToAdd);
+            newOccupied.add(timeString);
         }
+
+        return new Timetable(newMapping, newOccupied);
     }
 
 
 
     // Return all possible timetables with one more potential session (that has no conflict) added.
     public ArrayList<Timetable> extensions (ArrayList<NewCourse> courses){
-        ArrayList<Session> remainingSessions = this.neededSessions(courses);
+        Map<String, Set<String>> mapping = this.courseToSession();
+        ArrayList<Timetable> result = new ArrayList<>();
 
+        Set<Session> courseSessions = new HashSet<>();
+        for (NewCourse course : courses){
+            courseSessions.addAll(course.lectures);
+            courseSessions.addAll(course.labs);
+            courseSessions.addAll(course.tutorials);
+        }
+
+        for (Session s : courseSessions){
+            if (this.hasNoConflict(s, mapping)){
+                result.add(this.addSession(s));
+            }
+        }
+        return result;
     }
 }
-
