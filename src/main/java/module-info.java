a38efb58d69f5d8efc6c1063af72d77262b61121
PIAO-A-PIAO <piaoxuanyi2001@163.com>
2021-11-08 17:56:23 -0500

    Update of GUI related files after Ethan

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,12 @@
 module com.example.gui.main{
         requires javafx.controls;
         requires javafx.fxml;
+
+        requires org.controlsfx.controls;
+        requires com.dlsc.formsfx;
+        requires org.kordamp.ikonli.javafx;
+        requires org.kordamp.bootstrapfx.core;
+
         opens com.example.gui to javafx.fxml;
         exports com.example.gui;
         }
