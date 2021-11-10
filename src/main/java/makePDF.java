5b3f136aea85d8af5818af37780c23fa477bfa1a
Le119 <lewei.er@mail.utoronto.ca>
2021-11-10 00:47:08 -0600

    itext still does not work. Weird error I do not understand.

--- src/main/java/makePDF.java
+++ src/main/java/makePDF.java
@@ -1,3 +1,51 @@
-public class makePDF {
+/*
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.*;
 
+
+public class makePDF {
+    int HRS_OF_DAY = 12;
+    int DAYS_OF_WEEK = 5;
+    PdfPTable table = new PdfPTable(HRS_OF_DAY + 1);
+    PdfPCell cell = new PdfPCell(new Phrase("Cell with colspan 6"));
+    table.addCell(cell);
+}
+*/
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.FileOutputStream;
+
+public class makePDF {
+    public static void main(String[] args) {
+        Document document = new Document();
+
+        try {
+            PdfWriter.getInstance(document,
+                    new FileOutputStream("HelloWorld-Table.pdf"));
+
+            document.open();
+
+            PdfPTable table = new PdfPTable(3); // 3 columns.
+
+            PdfPCell cell1 = new PdfPCell(new Paragraph("Cell 1"));
+            PdfPCell cell2 = new PdfPCell(new Paragraph("Cell 2"));
+            PdfPCell cell3 = new PdfPCell(new Paragraph("Cell 3"));
+
+            table.addCell(cell1);
+            table.addCell(cell2);
+            table.addCell(cell3);
+
+            document.add(table);
+
+            document.close();
+        } catch(Exception e){
+
+        }
+    }
 }
