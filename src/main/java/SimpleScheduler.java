cef2fe3c5a96b2ec5c503b87e0f8d46b22b0c930
noaoch <jerry.yan@mail.utoronto.ca>
2021-11-11 11:12:14 -0500

    Added some testing in simple scheduler

--- src/main/java/SimpleScheduler.java
+++ src/main/java/SimpleScheduler.java
@@ -50,36 +50,36 @@ class SimpleScheduler {
     public static void main(String[] args) throws IOException {
         ArrayList<NewCourse> courses = new ArrayList<>();
 
-        Session csc207a = new Session("Paul Gries", "CSC207H1F", "LEC0101", new Integer[]{21314, 41314});
-        Session csc207b = new Session("Paul Gries", "CSC207H1F", "LEC0201", new Integer[]{21415, 41415});
-        Session csc207c = new Session("TBA", "CSC207H1F", "TUT0301", new Integer[]{11416});
+//        Session csc207a = new Session("Paul Gries", "CSC207H1F", "LEC0101", new Integer[]{21314, 41314});
+//        Session csc207b = new Session("Paul Gries", "CSC207H1F", "LEC0201", new Integer[]{21415, 41415});
+//        Session csc207c = new Session("TBA", "CSC207H1F", "TUT0301", new Integer[]{11416});
+//
+//        ArrayList<Session> csc207tut = new ArrayList<>();
+//        csc207tut.add(csc207c);
+//
+//        ArrayList<Session> csc207lec = new ArrayList<>();
+//        csc207lec.add(csc207a);
+//        csc207lec.add(csc207b);
+//
+//        courses.add(new NewCourse("CSC207H1F", csc207tut, csc207lec, new ArrayList<>()));
+//
+//
+//        Session mat224a = new Session("Soheil", "MAT224H1F", "LEC0101", new Integer[]{11011, 31011, 41011});
+//        Session mat224b = new Session("Jeffery", "MAT224H1F", "LEC0201", new Integer[]{11617, 31617, 41617});
+//        Session mat224c = new Session("TBA", "MAT224H1F", "TUT0102", new Integer[]{11112});
+//
+//        ArrayList<Session> mat224tut = new ArrayList<>();
+//        mat224tut.add(mat224c);
+//
+//        ArrayList<Session> mat224lec = new ArrayList<>();
+//        mat224lec.add(mat224a);
+//        mat224lec.add(mat224b);
+//
+//        courses.add(new NewCourse("MAT224H1F", mat224tut, mat224lec, new ArrayList<>()));
 
-        ArrayList<Session> csc207tut = new ArrayList<>();
-        csc207tut.add(csc207c);
-
-        ArrayList<Session> csc207lec = new ArrayList<>();
-        csc207lec.add(csc207a);
-        csc207lec.add(csc207b);
-
-        courses.add(new NewCourse("CSC207H1F", csc207tut, csc207lec, new ArrayList<>()));
-
-
-        Session mat224a = new Session("Soheil", "MAT224H1F", "LEC0101", new Integer[]{11011, 31011, 41011});
-        Session mat224b = new Session("Jeffery", "MAT224H1F", "LEC0201", new Integer[]{11617, 31617, 41617});
-        Session mat224c = new Session("TBA", "MAT224H1F", "TUT0102", new Integer[]{11112});
-
-        ArrayList<Session> mat224tut = new ArrayList<>();
-        mat224tut.add(mat224c);
-
-        ArrayList<Session> mat224lec = new ArrayList<>();
-        mat224lec.add(mat224a);
-        mat224lec.add(mat224b);
-
-        courses.add(new NewCourse("MAT224H1F", mat224tut, mat224lec, new ArrayList<>()));
-
-//        courses.add(WebParse.courseParse("CSC108H1F"));
-//        courses.add(WebParse.courseParse("PSY100H1F"));
-//        courses.add(WebParse.courseParse("MAT224H1F"));
+        courses.add(WebParse.courseParse("CSC108H1F"));
+        courses.add(WebParse.courseParse("PSY100H1F"));
+        courses.add(WebParse.courseParse("MAT224H1F"));
 
 
 
