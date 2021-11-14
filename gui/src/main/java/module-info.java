fb4e3cd2aabb00aedfcaf872b747da9cd495e645
PIAO-A-PIAO <piaoxuanyi2001@163.com>
2021-11-14 16:31:56 -0500

    edited module structure

--- gui/src/main/java/module-info.java
+++ gui/src/main/java/module-info.java
@@ -2,9 +2,11 @@ module com.example.gui {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.jsoup;
+    requires itextpdf;
 
     opens com.example.gui to javafx.fxml;
     exports com.example.gui;
     exports com.example.timetable;
     exports com.example.filter;
+    exports com.example.pdf;
 }
--- src/main/java/module-info.java
+++
@@ -1,5 +0,0 @@
-module modules.main {
-    requires com.example;
-
-    exports com.example.pdf;
-}
