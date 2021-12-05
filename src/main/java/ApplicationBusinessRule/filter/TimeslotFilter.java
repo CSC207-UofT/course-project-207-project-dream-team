7d652d4b17ab0dd43f3f244e8e08a600bbf695d1
czzcczz <ccchen.zhang@mail.utoronto.ca>
2021-12-04 19:09:44 -0500

    add javadoc for Filter classes answer question 2 for accessibility report

--- src/main/java/ApplicationBusinessRule/filter/TimeslotFilter.java
+++ src/main/java/ApplicationBusinessRule/filter/TimeslotFilter.java
@@ -6,10 +6,20 @@ import java.util.ArrayList;
 
 public class TimeslotFilter extends Filter {
 
+    /**
+     * constructor for TimeslotFilter
+     * @param input an Arraylist of timetable
+     * @param unwanted an Arraylist of unwanted timeslot
+     */
+
     public TimeslotFilter(ArrayList<Timetable> input, ArrayList<String> unwanted) {
         super(input, unwanted);
     }
 
+    /**
+     * sort out timetable with unwanted timeslot
+     * @return an Arraylist of timetable
+     */
     @Override
     public ArrayList<Timetable> sort() {
 
