53e03af2ac76b3ffe978d14add7f9b7055079009
Le119 <lewei.er@mail.utoronto.ca>
2021-12-08 21:04:15 -0500

    error resolved for Filter SimpleScheduler and Timetable

--- src/main/java/ApplicationBusinessRule/filter/Filter.java
+++ src/main/java/ApplicationBusinessRule/filter/Filter.java
@@ -59,33 +59,4 @@ public abstract class Filter {
     public ArrayList<String> getUnwanted() {
         return this.unwanted;
     }
-
-
-    // this method is created to meet the Design Pattern
-    // 1: InstructorFilter; 2: MaximumFilter; 3: TimeslotFilter
-//     public final ArrayList<Timetable> call(Integer type) {
-//         if (type == 1) {
-//             InstructorFilter aa = new InstructorFilter(this.input, this.unwanted);
-//             return aa.sort();
-//         }
-//        if (type == 2) {
-//             MaximumHourFilter aa = new MaximumHourFilter(this.input, this.unwanted);
-//             return aa.sort();
-//         }
-//         if (type == 3) {
-//             TimeslotFilter aa = new TimeslotFilter(this.input, this.unwanted);
-//             return aa.sort();
-//         }
-//         return new ArrayList<>(0);
-//     }
-
-    public final void isFiltered() {
-        try {
-            if (this.sort().size() > 0) {
-                System.out.println("Timetables are sucessfully filtered.");
-            }
-        } catch (Exception e) {
-            System.out.println("Timetable are not filtered.");
-        }
-    }
 }
